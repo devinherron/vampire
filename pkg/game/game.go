d7347f08aef5980a39f1d8fa354f2828f512384c
devinherron <devin@choi-herron.com>
2022-06-30 21:22:31 -0700

    Can now play through the game

--- pkg/game/game.go
+++ pkg/game/game.go
@@ -1,11 +1,58 @@
 package game
 
 import (
+	"fmt"
+
 	"github.com/devinherron/dice"
 )
 
 func NewGame(prompts [][]string) {
-	println(Roll())
+	visited := make([][]bool, 81)
+	finished := false
+
+	currentPrompt := 1
+	currentEntry := 0
+
+	for !finished {
+		result := Roll()
+		if currentPrompt+result < 1 {
+			currentPrompt = 1
+		} else {
+			currentPrompt += result
+		}
+
+		if len(visited[currentPrompt]) == 0 {
+			visited[currentPrompt] = make([]bool, 3)
+		}
+
+		for visited[currentPrompt][currentEntry] {
+			if currentEntry > 1 {
+				currentPrompt++
+				currentEntry = 0
+
+				if len(visited[currentPrompt]) == 0 {
+					visited[currentPrompt] = make([]bool, 3)
+				}
+			} else {
+				currentEntry++
+			}
+		}
+
+		fmt.Printf("[%d] %s\n", currentPrompt, prompts[currentPrompt][currentEntry])
+		visited[currentPrompt][currentEntry] = true
+
+		if currentPrompt > 71 {
+			finished = true
+		}
+
+		var input string
+		fmt.Scanln(&input)
+
+		if input == "quit" {
+			finished = true
+		}
+	}
+
 }
 
 func Roll() int {
